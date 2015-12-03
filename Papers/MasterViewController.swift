2fb8182dd072c21d60cb64b7c93e712096cd80e9
Pedro Ruíz <pedro.ruiz.diaz@me.com>
2015-12-04 01:59:27 +0900

    CollectionView Cell Selection implemented via Storyboard

---
+++ Papers/MasterViewController.swift
@@ -0,0 +1,49 @@
+//
+//  MasterViewController.swift
+//  Papers
+//
+//  Created by Pedro Ruíz on 12/3/15.
+//  Copyright © 2015 Pedro Ruíz. All rights reserved.
+//
+
+import UIKit
+
+class MasterViewController: UICollectionViewController {
+    private var papersDataSource = PapersDataSource()
+    
+    //MARK: - UIViewController
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let picturesPerRow = CGFloat(3)
+        let width = CGRectGetWidth(collectionView!.frame) / picturesPerRow
+        let layout = collectionViewLayout as! UICollectionViewFlowLayout
+        layout.itemSize = CGSize(width: width, height: width)
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == "MasterToDetail" {
+            if let indexPath = collectionView!.indexPathsForSelectedItems()!.first! as? NSIndexPath {
+                if let paper = papersDataSource.paperForItemAtIndexPath(indexPath) {
+                    let detailViewController = segue.destinationViewController as! DetailViewController
+                    detailViewController.paper = paper
+                }
+            }
+        }
+    }
+    
+    //MARK: - UICollectionViewDataSource
+    
+    override func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return papersDataSource.count
+    }
+    
+    override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("PaperCell", forIndexPath: indexPath) as UICollectionViewCell
+        return cell
+    }
+    
+}
